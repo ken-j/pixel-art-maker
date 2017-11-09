55eacf9d32526a69f4761f856295cfd02f664afd
ken-j <ken.j.morrissette@gmail.com>
2017-11-09 12:20:33 -0600

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
     console.log( "ready!" );
-    
+    // When size is submitted by the user
     function makeGrid(inputHeight, inputWidth) {
         let graphTable='';
         let inputColor= $('#colorPicker').val();
         $( "#colorPicker" ).on( "change", function() {
             inputColor = $('#colorPicker').val();
         });
+        // creating coloumns and rows with for loops
         for(i = 0; i < inputHeight; ++i) {
             graphTable=graphTable+'<tr>';
             for(j = 0; j < inputWidth; ++j) {
@@ -19,7 +20,7 @@ $(document).ready(function() {
             $( this ).css("background-color", inputColor);
         });
     }
-    
+    // Generating grid
     $( "#sizePicker" ).submit(function( event ) {
         event.preventDefault();
         $('#pixel_canvas').empty();
